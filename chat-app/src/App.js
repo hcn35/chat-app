8cebc1b1673a01e76f962113fa96b41702d6c19b
Hiep Nguyen <hiepsq8@gmail.com>
2022-10-20 22:41:18 -0400

    Finish Register route

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 export default function App() {
   return (
--- chat-app/src/pages/Register.jsx
+++ chat-app/src/pages/Register.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./Register.css";
 
 function Register() {
   const handleSubmit = (event) => {
@@ -10,28 +14,43 @@ function Register() {
   };
 
   return (
-    <Form onSubmit={(event) => handleSubmit(event)}>
-      <Form.Group className="mb-3" controlId="formBasicUserName">
-        <Form.Label>User Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter a name" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicConfirmedPassword">
-        <Form.Label>Confirmed Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter confirmed password" />
-      </Form.Group>
-      <Button type="submit">Create User</Button>
-      <div>
-        Already have an account? <Link to="/login">Login</Link>
-      </div>
-    </Form>
+    <Container id="main-container" className="d-grid h-100">
+      <Form
+        id="register-form"
+        className="w-100 p-4 shadow"
+        onSubmit={(event) => handleSubmit(event)}
+      >
+        <h1 className="text-center mb-4">Register</h1>
+        <Row className="mb-3">
+          <Form.Group as={Col} controlId="formGridFirstname">
+            <Form.Control type="text" placeholder="Firstname" />
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridLastname">
+            <Form.Control type="text" placeholder="Lastname" />
+          </Form.Group>
+        </Row>
+
+        <Form.Group className="mb-3" controlId="formGridEmail">
+          <Form.Control type="email" placeholder="Email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formGridPassword">
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group className="mb-4" controlId="formGridConfirmedPassword">
+          <Form.Control type="password" placeholder="Confirmed Password" />
+        </Form.Group>
+
+        <Button className="w-100" type="submit">
+          Create User
+        </Button>
+        <div className="text-center text-muted">
+          Already have an account? <Link to="/login">Login</Link>
+        </div>
+      </Form>
+    </Container>
   );
 }
 
