301b566dda1d8fc1019bf1b3d6430bab7431bfcb
Hiep Nguyen <hiepsq8@gmail.com>
2022-10-26 16:54:52 -0400

    Add toastify

--- chat-app/src/pages/Register.jsx
+++ chat-app/src/pages/Register.jsx
@@ -6,6 +6,8 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./Register.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Register() {
   const validEmailRegex = RegExp(
@@ -27,9 +29,39 @@ function Register() {
     },
   });
 
+  const validatation = (userData) => {
+    const copyUserData = { ...userData };
+    if (
+      copyUserData.firstname &&
+      copyUserData.lastname &&
+      copyUserData.email &&
+      copyUserData.password &&
+      copyUserData.confirmPassword &&
+      copyUserData.errors.firstname === "" &&
+      copyUserData.errors.lastname === "" &&
+      copyUserData.errors.email === "" &&
+      copyUserData.errors.password === "" &&
+      copyUserData.errors.confirmPassword === ""
+    ) {
+      return true;
+    }
+    return false;
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert("You have submitted the form.");
+    if (validatation(state)) {
+      alert("Valid Form");
+    } else {
+      toast.error("Invalid Form", {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 3000,
+        pauseOnHover: true,
+        draggable: false,
+        theme: "light",
+        hideProgressBar: true,
+      });
+    }
   };
 
   const handleChange = (event) => {
@@ -38,10 +70,10 @@ function Register() {
     let errors = state.errors;
     switch (name) {
       case "firstname":
-        errors.firstname = value.length < 5 ? "At least 5 characters!" : "";
+        errors.firstname = value.length < 1 ? "At least 1 characters!" : "";
         break;
       case "lastname":
-        errors.lastname = value.length < 5 ? "At least 5 characters!" : "";
+        errors.lastname = value.length < 1 ? "At least 1 characters!" : "";
         break;
       case "email":
         errors.email = validEmailRegex.test(value) ? "" : "Invalid Email!";
@@ -63,83 +95,86 @@ function Register() {
   const { errors } = state;
 
   return (
-    <Container id="main-container" className="d-grid h-100">
-      <Form
-        id="register-form"
-        className="w-100 p-4 shadow"
-        onSubmit={(event) => handleSubmit(event)}
-      >
-        <h1 className="text-center mb-4">Register</h1>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridFirstname">
+    <>
+      <Container id="main-container" className="d-grid h-100">
+        <Form
+          id="register-form"
+          className="w-100 p-4 shadow"
+          onSubmit={(event) => handleSubmit(event)}
+        >
+          <h1 className="text-center mb-4">Register</h1>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridFirstname">
+              <Form.Control
+                name="firstname"
+                type="text"
+                placeholder="Firstname"
+                onChange={(event) => handleChange(event)}
+              />
+              {errors.firstname.length > 0 && (
+                <span className="error">{errors.firstname}</span>
+              )}
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridLastname">
+              <Form.Control
+                name="lastname"
+                type="text"
+                placeholder="Lastname"
+                onChange={(event) => handleChange(event)}
+              />
+              {errors.lastname.length > 0 && (
+                <span className="error">{errors.lastname}</span>
+              )}
+            </Form.Group>
+          </Row>
+
+          <Form.Group className="mb-3" controlId="formGridEmail">
             <Form.Control
-              name="firstname"
-              type="text"
-              placeholder="Firstname"
+              name="email"
+              type="email"
+              placeholder="Email"
               onChange={(event) => handleChange(event)}
             />
-            {errors.firstname.length > 0 && (
-              <span className="error">{errors.firstname}</span>
+            {errors.email.length > 0 && (
+              <span className="error">{errors.email}</span>
             )}
           </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridLastname">
+          <Form.Group className="mb-3" controlId="formGridPassword">
             <Form.Control
-              name="lastname"
-              type="text"
-              placeholder="Lastname"
+              name="password"
+              type="password"
+              placeholder="Password"
               onChange={(event) => handleChange(event)}
             />
-            {errors.lastname.length > 0 && (
-              <span className="error">{errors.lastname}</span>
+            {errors.password.length > 0 && (
+              <span className="error">{errors.password}</span>
             )}
           </Form.Group>
-        </Row>
 
-        <Form.Group className="mb-3" controlId="formGridEmail">
-          <Form.Control
-            name="email"
-            type="email"
-            placeholder="Email"
-            onChange={(event) => handleChange(event)}
-          />
-          {errors.email.length > 0 && (
-            <span className="error">{errors.email}</span>
-          )}
-        </Form.Group>
+          <Form.Group className="mb-4" controlId="formGridConfirmedPassword">
+            <Form.Control
+              name="confirmPassword"
+              type="password"
+              placeholder="Confirmed Password"
+              onChange={(event) => handleChange(event)}
+            />
+            {errors.confirmPassword.length > 0 && (
+              <span className="error">{errors.confirmPassword}</span>
+            )}
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formGridPassword">
-          <Form.Control
-            name="password"
-            type="password"
-            placeholder="Password"
-            onChange={(event) => handleChange(event)}
-          />
-          {errors.password.length > 0 && (
-            <span className="error">{errors.password}</span>
-          )}
-        </Form.Group>
-
-        <Form.Group className="mb-4" controlId="formGridConfirmedPassword">
-          <Form.Control
-            name="confirmPassword"
-            type="password"
-            placeholder="Confirmed Password"
-            onChange={(event) => handleChange(event)}
-          />
-          {errors.confirmPassword.length > 0 && (
-            <span className="error">{errors.confirmPassword}</span>
-          )}
-        </Form.Group>
-
-        <Button className="w-100" type="submit">
-          Create User
-        </Button>
-        <div className="text-center text-muted">
-          Already have an account? <Link to="/login">Login</Link>
-        </div>
-      </Form>
-    </Container>
+          <Button className="w-100" type="submit">
+            Create User
+          </Button>
+          <div className="text-center text-muted">
+            Already have an account? <Link to="/login">Login</Link>
+          </div>
+        </Form>
+      </Container>
+      <ToastContainer />
+    </>
   );
 }
 
